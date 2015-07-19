9e6e296c51e73885eb29a1d462a6c5fa8255d4f5
Simon Kurka <dev@simon.kurka.cc>
2015-07-20 00:20:07 +0200

    Some cairo, writing Code under barcode

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,9 +13,8 @@ class ItemsController < ApplicationController
     respond_to do |format|
       format.html
       format.json
-      format.pdf { render body: get_barcode.to_pdf, content_type: 'application/pdf' }
-      format.svg { render body: get_barcode.to_svg, content_type: 'image/svg+xml' }
-      format.png { render body: get_barcode.to_png, content_type: 'image/png' }
+      format.pdf { render body: to_pdf, content_type: 'application/pdf' }
+      format.png { render body: to_png, content_type: 'image/png' }
     end
   end
 
@@ -86,5 +85,65 @@ class ItemsController < ApplicationController
       barcode = Barby::Code39.new(@item.id.to_s(36).upcase.rjust(4, '0'))
       barcode.include_checksum = true
       barcode
+    end
+    
+    def to_png
+      output_to_string_io do |io|
+        Cairo::ImageSurface.new(nil, 101 * dimen(:png), 54 * dimen(:png)) do |surface|
+          render_cairo(surface, :png, 101, 54)
+          surface.write_to_png(io)
+        end
+      end
+    end
+
+    def to_pdf
+      output_to_string_io do |io|
+        Cairo::PDFSurface.new(io, 101 * dimen(:pdf), 54 * dimen(:pdf)) do |surface|
+          render_cairo(surface, :pdf, 101, 54)
+        end
+      end
+    end
+
+    def dimen(format)
+      # 1 Point = 1/72 inch in cairo
+      # 1 Point = 1/96 inch in many browsers
+      if format == :pdf
+        dpi = 72
+      else
+        dpi = 96
+      end
+      # 1 Inch = 25.4 mm
+      (dpi / 25.4)
+    end
+
+    def render_cairo(surface, format, width, height)
+      cr = Cairo::Context.new(surface)
+      yield(cr) if block_given?
+      cr.set_source_color(:white)
+      cr.paint
+
+      barcode = get_barcode
+      outputter = barcode.outputter_for :render_to_cairo_context
+
+      extents = cr.text_extents(barcode.data);
+
+      options = {:margin => 0, :x => (width - 5) * dimen(format) - outputter.width, :y => 2.5 * dimen(format), :height => (height - 5) * dimen(format) - extents.height - dimen(format)}
+      outputter.render_to_cairo_context(cr, options)
+
+      cr.select_font_face("Georgia", Cairo::FONT_SLANT_NORMAL, Cairo::FONT_WEIGHT_BOLD)
+      cr.set_font_size(12)
+      cr.set_source_rgb(0, 0, 0)
+
+      cr.move_to(outputter.x(options) + ((outputter.width(options) - extents.width) / 2), outputter.y(options) + outputter.height(options) + extents.height + dimen(format))
+      cr.show_text(barcode.data)
+
+      cr
+    end
+
+    def output_to_string_io
+      io = StringIO.new
+      yield(io)
+      io.rewind
+      io.read
     end
 end
