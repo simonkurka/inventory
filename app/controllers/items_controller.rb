b82ac331898cfb8269664b65fa86b8939f055528
TheDM <dominik.martens@web.de>
2015-08-14 15:18:51 +0200

    lable generating throu LaTeX possible design bust be improved

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -67,6 +67,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def lableBig
+    @item = Item.find(params[:id])
+  end
+
+  def lableSmall
+    @item = Item.find(params[:id])
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
@@ -146,4 +153,5 @@ class ItemsController < ApplicationController
       io.rewind
       io.read
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items
+  resources :items do
+    member do
+      get 'lableBig'
+      get 'lableSmall'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
